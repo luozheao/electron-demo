baeb0bea7aa5cf86e204d0fef2d8847c77edb90b
罗哲傲 <zheao.luo@pjbest.com>
2018-09-13 10:42:40 +0800

    save

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const {BrowserWindow,app} = require('electron')
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
         width: 1500,
-        height:800
+        height:900
     }); 
     mainWindow.loadURL('file://' + __dirname + '/doc/index.html');
 
